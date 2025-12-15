f76c43fdc5ae309f652157990a9c022e4ea89c08
PanderzGG <saschakleinen@proton.me>
2025-12-15 10:29:14 +0100

    added HttpNavigation strings and AppDefault Styles

---
+++ UsefulData/HttpNavigation/HttpNavigation.cs
@@ -0,0 +1,11 @@
+﻿namespace LMDriversDash.UsefulData.HttpNavigation;
+
+public class HttpNavigation
+{
+    public const string GetSessionGameState = "/rest/sessions/GetGameState";
+    public const string GetSessionInfoForEvent = "/rest/sessions/GetSessionsInfoForEvent";
+    public const string GetSessionAmount = "/rest/sessions/amount";
+    public const string GetSessionWeather = "/rest/sessions/weather";
+    public const string GetNavigationState = "/navigation/state";
+    public const string GetProfileInfo = "/rest/profile/profileInfo/getProfileInfo";
+}
