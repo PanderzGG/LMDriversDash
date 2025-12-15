2fd87736efb2d0d28479062876ff254f705dd472
PanderzGG <saschakleinen@proton.me>
2025-12-15 10:24:59 +0100

    Small Push for Navigation States as const. Avoiding using those as magic strings. Debateable if they need to be in a static class

---
+++ UsefulData/GameState/HttpGameStates.cs
@@ -0,0 +1,8 @@
+﻿namespace LMDriversDash.UsefulData.GameState;
+
+public static class HttpGameStates
+{
+    public const string PlayerOnTrack = "NAV_REALTIME";
+    public const string PlayerInPits = "NAV_EVENT";
+    public const string PlayerInMainMenu = "NAV_MAIN_MENU";
+}
