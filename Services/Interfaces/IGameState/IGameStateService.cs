19046c477c4f2bfc53d5ba1834554d1f09e2a08f
PanderzGG <saschakleinen@proton.me>
2025-12-15 10:41:42 +0100

    started working on gamestate and clients.

--- Services/Interfaces/IGameState/IGameStateService.cs
+++ Services/Interfaces/IGameState/IGameStateService.cs
@@ -1,6 +1,16 @@
-﻿namespace LMDriversDash.Services.Interfaces.IGameState;
+﻿using System.Threading.Tasks;
+using LMDriversDash.Models.HttpModels.GameState;
+using LMDriversDash.Models.HttpModels.InfoModels;
+
+namespace LMDriversDash.Services.Interfaces.IGameState;
 
 public interface IGameStateService
 {
-    
+    // Needed Properties
+    LoadingStatus? LoadingStatus { get; set; }
+    State? CurrentState { get; set; }
+    HttpProfileInfo? HttpProfileInfo { get; set; }
+
+    Task RunOnStartupAsync();
+    void Stop();
 }
--- Services/Service/GameState/GameStateService.cs
+++ Services/Service/GameState/GameStateService.cs
@@ -1,8 +1,43 @@
-﻿using LMDriversDash.Services.Interfaces.IGameState;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using LMDriversDash.Models.HttpModels.GameState;
+using LMDriversDash.Models.HttpModels.InfoModels;
+using LMDriversDash.Services.Interfaces.IClients;
+using LMDriversDash.Services.Interfaces.IGameState;
 
 namespace LMDriversDash.Services.Service.GameState;
 
 public class GameStateService : IGameStateService
 {
+    private readonly IHttpClientService _httpClientService;
+    private readonly IUdpClientService _udpClientService;
     
+    // Cancellation Token to be abort infinite loop
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+    
+    // Polling Interval for Http
+    private TimeSpan _pollInterval = TimeSpan.FromSeconds(1);
+    
+    public LoadingStatus? LoadingStatus { get; set; }
+    
+    public State? CurrentState { get; set; }
+    
+    public HttpProfileInfo? HttpProfileInfo { get; set; }
+
+    public GameStateService(IHttpClientService httpClientService, IUdpClientService udpClientService)
+    {
+        _httpClientService = httpClientService;
+        _udpClientService = udpClientService;
+    }
+    
+    public Task RunOnStartupAsync()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new System.NotImplementedException();
+    }
 }
