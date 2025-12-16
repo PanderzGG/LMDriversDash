3360de9317f05a193651d934fb4c038bd00260e4
PanderzGG <saschakleinen@proton.me>
2025-12-16 17:14:04 +0100

    Added More Http DataContext.

---
+++ Models/HttpModels/InSessionData/HttpStandings/SData/StandingsVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LMDriversDash.Models.HttpModels.InSessionData.HttpStandings.SData;
+
+public class StandingsVelocity
+{
+    [JsonPropertyName("velocity")]
+    public double Velocity { get; set; }
+
+    [JsonPropertyName("x")]
+    public double X { get; set; }
+
+    [JsonPropertyName("y")]
+    public double Y { get; set; }
+
+    [JsonPropertyName("z")]
+    public double Z { get; set; }
+}
+
---
+++ Models/HttpModels/InSessionData/HttpStandings/SWrapper/StandingsWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using LMDriversDash.Models.HttpModels.InSessionData.HttpStandings.SData;
+
+namespace LMDriversDash.Models.HttpModels.InSessionData.HttpStandings.SWrapper;
+
+/// <summary>
+/// Wrapper-Klasse für die Standings-Daten.
+/// Die API gibt ein direktes Array zurück, daher wird diese Klasse
+/// als List&lt;StandingsEntry&gt; deserialisiert.
+/// </summary>
+public class StandingsWrapper : List<StandingsEntry>
+{
+}
