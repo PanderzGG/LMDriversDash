9f6b147b0ec25427b6ed244fbcf162694e0dd61f
PanderzGG <saschakleinen@proton.me>
2025-12-16 16:41:16 +0100

    More Wrapper stuff to get data from the HTTP

---
+++ Models/HttpModels/InSessionData/PitStopTimeEstimate.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LMDriversDash.Models.HttpModels.InSessionData;
+
+public class PitStopTimeEstimate
+{
+    [JsonPropertyName("damage")]
+    public double Damage { get; set; }
+    [JsonPropertyName("driverSwap")]
+    public double DriverSwap { get; set; }
+    [JsonPropertyName("fuel")]
+    public double Fuel { get; set; }
+    [JsonPropertyName("penalties")]
+    public double Penalties { get; set; }
+    [JsonPropertyName("tires")]
+    public double Tires { get; set; }
+    [JsonPropertyName("total")]
+    public double Total { get; set; }
+    [JsonPropertyName("ve")]
+    public double Ve { get; set; }
+    
+}
---
+++ Models/HttpModels/InSessionData/SessionSetup/SSData/SSWeatherNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LMDriversDash.Models.HttpModels.InSessionData.SessionSetup.SSData;
+
+public class SSWeatherNode
+{
+    [JsonPropertyName("Duration")]
+    public int Duration { get; set; }
+
+    [JsonPropertyName("Humidity")]
+    public int Humidity { get; set; }
+
+    [JsonPropertyName("RainChance")]
+    public int RainChance { get; set; }
+
+    [JsonPropertyName("Sky")]
+    public int Sky { get; set; }
+
+    [JsonPropertyName("StartTime")]
+    public int StartTime { get; set; }
+
+    [JsonPropertyName("Temperature")]
+    public int Temperature { get; set; }
+
+    [JsonPropertyName("WindDirection")]
+    public int WindDirection { get; set; }
+
+    [JsonPropertyName("WindSpeed")]
+    public int WindSpeed { get; set; }
+}
+
