6e9c6b29aec42f5bf7238e547d863b11c5b44e79
PanderzGG <saschakleinen@proton.me>
2025-12-17 14:09:35 +0100

    Some More UI Work for the Home Tab. Also implented temporary test data on the Designer for HomeView

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
 using LMDriversDash.Services.Events;
 using LMDriversDash.Services.Interfaces.IGameState;
@@ -16,6 +18,8 @@ public partial class HomeViewModel : PageViewModel
     [ObservableProperty] private string _httpConnectionColor = "#fc0328";
     [ObservableProperty] private string _UdpConnectionColor = "#fc0328";
     
+    // TestData for UI Mockup
+    public ObservableCollection<HomeGridTestData> gridtest { get; }
     // In Class attributes
     private bool httpIsConnected = false;
     private bool udpIsConnected = false;
@@ -51,5 +55,34 @@ public partial class HomeViewModel : PageViewModel
     public HomeViewModel()
     {
         PageName = ApplicationPageNames.Home;
+        var test = new List<HomeGridTestData>
+        {
+            new HomeGridTestData("01.01.2026","Spa","2:00","1","Won","x, x"),
+            new HomeGridTestData("01.01.2026","Spa","2:01","2","Podium","x, x"),
+            new HomeGridTestData("01.01.2026","Spa","2:02","3","Podium","x, x"),
+            new HomeGridTestData("01.01.2026","Spa","2:03","4","Top 5","x, x"),
+            new HomeGridTestData("01.01.2026","Spa","2:04","5","Top 5","x, x")
+        };
+        gridtest = new ObservableCollection<HomeGridTestData>(test);
+    }
+}
+
+public class HomeGridTestData
+{
+    public string? Date { get; set;}
+    public string? Track { get; set;}
+    public string? BestLap { get; set;}
+    public string? Position { get; set;}
+    public string? Status { get; set;}
+    public string? Actions { get; set;}
+
+    public HomeGridTestData(string date, string track, string bestLap, string position, string status, string actions)
+    {
+        Date = date;
+        Track = track;
+        BestLap = bestLap;
+        Position = position;
+        Status = status;
+        Actions = actions;
     }
 }
